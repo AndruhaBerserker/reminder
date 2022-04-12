11bbe2ec340ec3dc34026a73627ac0a4970176bc
AndruhaBerserker <96764865+AndruhaBerserker@users.noreply.github.com>
2022-04-12 15:15:24 +0300

    fixed some grammar

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main(set_time):
         time.sleep(set_time)
         playsound('beep.wav')
         notify = Label(clock,
-                       text="Do an exersise and push the button",
+                       text="Do an exercises and push the button",
                        bg="black", fg="red",
                        font=("Arial", 10, "bold")).place(x=50, y=80)
         time.sleep(2)
@@ -33,10 +33,4 @@ clock.title("Reminder")
 clock.geometry("300x120")
 hour_time= Button(clock, text ="30 min",fg="red",width =10,command = actual30).place(x=50,y=30)
 min_time = Button(clock, text="60 min", fg="red", width=10, command= actual60).place(x=150, y=30)
-
 clock.mainloop()
-
-
-
-
-
